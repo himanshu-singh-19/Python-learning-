a9fe25702753067019c3acdb0ae36bc5e88e2e98
Himan0000050 <himanshusingh19808@gmail.com>
2026-05-28 18:48:15 +0530

    first commit

---
+++ basic2.py
@@ -0,0 +1,26 @@
+name = "Himanshu"
+print("Hello", name)
+a = 14
+b = 18
+
+print(a + b)
+age = 18
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote yet")
+    
+
+number = 19    
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+    
+    number = int(input("10"))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
---
+++ hello.py
@@ -0,0 +1,52 @@
+# a = "18"
+# b = 12
+# print(a + b)
+
+
+
+
+# a = "18"
+# b = "12"
+# print(a + b)
+
+
+
+
+
+# age = input("Enter a age: ")
+
+# age = int(age)
+
+# if age < 18:
+#     print("The age is can't vote")
+# else:
+#     print("The age is eligible to vote")
+#     for i in range(4):
+#         print("Himanshu")
+
+
+
+#multiline string not possible with "" or ''
+name = "pranay himanshu"
+
+name = '''
+Fruits
+    banana
+    apple
+Vegetables
+    ladij finger
+    bangan
+
+
+
+'''
+
+print(name)
+
+
+
+
+
+
+
+
